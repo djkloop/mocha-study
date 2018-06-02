d2fb7d91ddf9f1cea1ab4a392806297c2db98c5b
djkloop <1044032256@qq.com>
2018-06-02 19:02:48 +0800

    update

---
+++ calc.js
@@ -0,0 +1,8 @@
+/*
+  计算数值之和
+*/
+function add(x, y) {
+  return x - y;
+}
+
+module.exports.add = add;
---
+++ index.js
@@ -0,0 +1,8 @@
+const add = require('./calc.js').add;
+
+var x  = 9;
+var y = 10;
+
+var z = add(x, y);
+
+console.log(z);
